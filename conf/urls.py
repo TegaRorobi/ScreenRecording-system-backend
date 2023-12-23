fb98f6ceb4d0fcb21f8c61386efa8bb38e611bff
Tega Rorobi <rorobitega@gmail.com>
2023-12-23 20:30:19 +0100

    feat: Added a urlconf in the 'main' app

--- conf/urls.py
+++ conf/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+
+api_version = 'v1'
 
 urlpatterns = [
     path(settings.ADMIN_URL + '/', admin.site.urls),
+    path(f'api/{api_version}/', include('main.urls')),
 ]
---
+++ main/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import re_path
+from .views import *
+
+app_name = 'main'
+
+urlpatterns = [
+
+]
