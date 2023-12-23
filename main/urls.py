0e4fc56adf51424fbcdb7d01c4d6e2cbfa373d51
Tega Rorobi <rorobitega@gmail.com>
2023-12-23 22:32:58 +0100

    edit: Registered the VideoViewSet's 'create_video' action in the 'main' urlconf

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 
 urlpatterns = [
     re_path('^videos/?$', VideoViewSet.as_view({'get':'retrieve_videos'}), name='retrieve-videos'),
+    re_path('^videos/create/?$', VideoViewSet.as_view({'post':'create_video'}), name='create-video'),
 ]
