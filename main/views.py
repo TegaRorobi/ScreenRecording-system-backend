fcda71fd25d0c7cbe66e39af21c824cf71eec7dc
Tega Rorobi <rorobitega@gmail.com>
2023-12-24 23:38:50 +0100

    feat: Added some code to the 'append_video' action on the VideoViewSet. (to be subsequently updated)

--- main/views.py
+++ main/views.py
@@ -1,23 +1,37 @@
 
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework import status, decorators
 from django.http import Http404
+
 from .serializers import *
 from .models import *
 from conf.utils import *
 
+import tempfile, os
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
 __all__ = 'VideoViewSet',
+
 
 class VideoViewSet(GenericViewSet):
 
     def get_queryset(self):
         return Video.objects.all()
+
     def get_serializer_class(self):
         if self.action in ('retrieve_videos', 'create_video'):
             return VideoSerializer
+        elif self.action == 'append_video':
+            return VideoChunkSerializer
+
     pagination_class = PaginatorGenerator()()
+
+    def remove_paths(self, *filepaths):
+        for fpath in filepaths:
+            os.remove(fpath)
+
 
     @decorators.action(detail=False)
     def retrieve_videos(self, request, *args, **kwargs):
@@ -29,6 +43,7 @@ class VideoViewSet(GenericViewSet):
         serializer = self.get_serializer(videos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
     @decorators.action(detail=True)
     def create_video(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -37,13 +52,61 @@ class VideoViewSet(GenericViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
     @decorators.action(detail=True)
     def append_video(self, request, *args, **kwargs):
-        pass
+
+        try:
+            video = self.get_object()
+        except Http404:
+            return Response({
+                'error': f'Invalid {self.lookup_url_kwarg or self.lookup_field}. Video with '
+                         f"{self.lookup_field} {kwargs.get('pk')!r} does not exist."
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(video=video)
+            chunk_file = serializer.validated_data['chunk_file']
+            video_filename = f'{video.id}.mp4'
+
+            if not video.video_file:
+                video.video_file.save(video_filename, chunk_file)
+
+            else:
+                existing_clip_tempfile = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
+                existing_clip_tempfile.write(video.video_file.read())
+                existing_clip_tempfile.seek(0)
+                existing_clip_tempfile_path = existing_clip_tempfile.name
+                existing_clip = VideoFileClip(existing_clip_tempfile_path)
+                # print(existing_clip_tempfile, existing_clip_tempfile_path, existing_clip, '\n\n', sep='\n')
+
+                new_clip_tempfile =  tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
+                new_clip_tempfile.write(chunk_file.read())
+                new_clip_tempfile.seek(0)
+                new_clip_tempfile_path = new_clip_tempfile.name
+                new_clip = VideoFileClip(new_clip_tempfile_path)
+                # print(new_clip_tempfile, new_clip_tempfile_path, new_clip, '\n\n', sep='\n')
+
+                final_clip_tempfile = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
+                final_clip = concatenate_videoclips([existing_clip, new_clip])
+                final_clip_tempfile_path = final_clip_tempfile.name
+                final_clip.write_videofile(final_clip_tempfile_path)
+                # print(final_clip_tempfile, final_clip_tempfile.name, final_clip, '\n\n', sep='\n')
+
+                final_clip_tempfile.seek(0)
+                video.video_file.save(video_filename, final_clip_tempfile)
+
+                self.remove_paths(existing_clip_tempfile_path, new_clip_tempfile_path, final_clip_tempfile_path)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     @decorators.action(detail=True)
     def stream_video(self, request, *args, **kwargs):
         pass
+
 
     @decorators.action(detail=True)
     def delete_video(self, request, *args, **kwargs):
