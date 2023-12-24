a8a860071d852f967e7c503251e906cd3711b272
Tega Rorobi <rorobitega@gmail.com>
2023-12-24 11:11:56 +0100

    edit: Registered the VideoViewSet's 'delete_video' action in the 'main' urlconf

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,20 @@ from .views import *
 app_name = 'main'
 
 urlpatterns = [
-    re_path('^videos/?$', VideoViewSet.as_view({'get':'retrieve_videos'}), name='retrieve-videos'),
-    re_path('^videos/create/?$', VideoViewSet.as_view({'post':'create_video'}), name='create-video'),
+    re_path(
+        '^videos/?$',
+        VideoViewSet.as_view({
+            'get':'retrieve_videos',
+            'post': 'create_video'
+        }),
+        name='retrieve-create-videos'
+    ),
+
+    re_path(
+        '^videos/(?P<pk>[a-zA-Z0-9\-]+)/?$',
+        VideoViewSet.as_view({
+            'delete':'delete_video',
+        }),
+        name='delete-video'
+    ),
 ]
